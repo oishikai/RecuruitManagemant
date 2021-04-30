1920715323a41b7777265617d6c1675a14d6be9b
Kai Oishi <bokuchan.kai@gmail.com>
2021-04-30 14:03:53 +0900

    firstcommit

--- RecuruitManagement/ViewController.swift
+++ RecuruitManagement/ViewController.swift
@@ -7,13 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var comp = ["A社", "B社", "C社"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = comp[indexPath.row]
+        return cell
+    }
 }
 
