75613290115c5d2c8ca57ac40acbd5a6f6fc7502
Kai Oishi <bokuchan.kai@gmail.com>
2021-04-30 13:46:13 +0900

    Initial Commit

---
+++ RecuruitManagement/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecuruitManagement
+//
+//  Created by Kai on 2021/04/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
