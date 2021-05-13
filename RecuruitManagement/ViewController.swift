ac488f75d04a7fc3d3c7e0a81ab6c59d93339cae
Kai Oishi <bokuchan.kai@gmail.com>
2021-05-13 22:20:49 +0900

    CoredataでCompany型を追加

--- RecuruitManagement/ViewController.swift
+++ RecuruitManagement/ViewController.swift
@@ -6,13 +6,24 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var comp = ["A社", "B社", "C社"]
+    var companies:[Company] = []
+    
+    var managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let dataCondition = NSFetchRequest<NSFetchRequestResult>(entityName: "Company")
+            do{
+              companies = try managedObjectContext.fetch(dataCondition) as! [Company]
+            }catch{
+              print("エラーだよ")
+            }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
