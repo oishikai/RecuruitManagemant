66a6f5c34b7d51c7172ef591f363d832948e8ed5
Kai Oishi <bokuchan.kai@gmail.com>
2021-05-15 17:05:03 +0900

    イベント追加画面のtextfieldのdelegateを設定

--- RecuruitManagement/ViewControllers/AddEventViewController.swift
+++ RecuruitManagement/ViewControllers/AddEventViewController.swift
@@ -7,9 +7,17 @@
 
 import UIKit
 
-class AddEventViewController: UIViewController {
+class AddEventViewController: UIViewController ,UITextFieldDelegate {
 
+    @IBOutlet weak var eventName: UITextField!
+    @IBOutlet weak var addNewEvent: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "イベントを追加する"
+
+        eventName.delegate = self
+    }
+    @IBAction func addNewEvent(_ sender: Any) {
     }
 }
