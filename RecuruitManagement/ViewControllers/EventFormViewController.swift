7bbb8777f18ffb87efb04137941db697c8d60b16
Kai Oishi <bokuchan.kai@gmail.com>
2021-05-18 00:45:54 +0900

    commit忘れ

--- RecuruitManagement/ViewControllers/EventFormViewController.swift
+++ RecuruitManagement/ViewControllers/EventFormViewController.swift
@@ -6,24 +6,27 @@
 //
 
 import UIKit
+import Eureka
 
-class ViewController: UIViewController {
+class EventFormViewController: FormViewController{
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        form +++ Section("Section1")
+                    <<< TextRow(){ row in
+                        row.title = "Text Row"
+                        row.placeholder = "Enter text here"
+                    }
+                    <<< PhoneRow(){
+                        $0.title = "Phone Row"
+                        $0.placeholder = "And numbers here"
+                    }
+                +++ Section("Section2")
+                    <<< DateRow(){
+                        $0.title = "Date Row"
+                        $0.value = Date(timeIntervalSinceReferenceDate: 0)
+                    }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
