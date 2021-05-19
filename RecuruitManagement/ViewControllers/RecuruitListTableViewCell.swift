62663b890aa684d0915038817b9de95be2b72e2f
Kai Oishi <bokuchan.kai@gmail.com>
2021-05-19 10:42:46 +0900

    カスタムセルに志望度を表すstarを表示できるようにした

--- RecuruitManagement/ViewControllers/RecuruitListTableViewCell.swift
+++ RecuruitManagement/ViewControllers/RecuruitListTableViewCell.swift
@@ -12,9 +12,15 @@ class RecuruitListTableViewCell: UITableViewCell {
     @IBOutlet weak var companyIcon: UIImageView!
     @IBOutlet weak var companyName: UILabel!
     @IBOutlet weak var selectionStatus: UILabel!
-    @IBOutlet weak var starImage: UIImageView!
     
-    var aspiration = ""
+    @IBOutlet weak var star1Image: UIImageView!
+    @IBOutlet weak var star2Image: UIImageView!
+    @IBOutlet weak var star3Image: UIImageView!
+    @IBOutlet weak var star4Image: UIImageView!
+    @IBOutlet weak var star5Image: UIImageView!
+    
+    var aspiration:Int = 0
+    
     static let cellIdentifier = String(describing: RecuruitListTableViewCell.self)
 
     let star = UIImage(named: "AspirationStar")
@@ -22,7 +28,13 @@ class RecuruitListTableViewCell: UITableViewCell {
     func setup(company:Company) {
         companyName.text = company.companyName
         selectionStatus.text = company.selectionStatus
-        aspiration = company.aspiration!
+        aspiration = Int(string: company.aspiration!) ?? 0
+        
+        star1Image.image = star
+        star2Image.image = star
+        star3Image.image = star
+        star4Image.image = star
+        star5Image.image = star
         
     }
     
