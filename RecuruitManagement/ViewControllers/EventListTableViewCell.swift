0866ea9902195de356a3a10bd232691477311cb4
Kai Oishi <bokuchan.kai@gmail.com>
2021-05-21 00:19:31 +0900

    image、labelの紐付け

--- RecuruitManagement/ViewControllers/EventListTableViewCell.swift
+++ RecuruitManagement/ViewControllers/EventListTableViewCell.swift
@@ -8,7 +8,17 @@
 import UIKit
 
 class EventListTableViewCell: UITableViewCell {
-
+    
+    @IBOutlet weak var eventIcon: UIImageView!
+    @IBOutlet weak var dateIcon: UIImageView!
+    @IBOutlet weak var locateIcon: UIImageView!
+    @IBOutlet weak var memoIcon: UIImageView!
+    
+    @IBOutlet weak var eventNameLabel: UILabel!
+    @IBOutlet weak var eventDateLabel: UILabel!
+    @IBOutlet weak var eventLocateLabel: UILabel!
+    
+    @IBOutlet weak var eventMemoField: UITextView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
