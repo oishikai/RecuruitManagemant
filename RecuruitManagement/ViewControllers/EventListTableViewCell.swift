1f22f087592bdba8b52aaa9ccd741f24a5eac2c3
Kai Oishi <bokuchan.kai@gmail.com>
2021-05-21 00:13:16 +0900

    イベントリストのカスタムセルファイル追加

---
+++ RecuruitManagement/ViewControllers/EventListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EventListTableViewCell.swift
+//  RecuruitManagement
+//
+//  Created by Kai on 2021/05/20.
+//
+
+import UIKit
+
+class EventListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
