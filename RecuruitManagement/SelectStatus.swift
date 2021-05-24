f5b4f7e6c4d1adbf64845a8955c785204b56c794
Kai Oishi <bokuchan.kai@gmail.com>
2021-05-24 22:46:55 +0900

    enum SelectionStatusファイルを追加

---
+++ RecuruitManagement/SelectStatus.swift
@@ -0,0 +1,16 @@
+//
+//  SelectStatus.swift
+//  RecuruitManagement
+//
+//  Created by Kai on 2021/05/24.
+//
+
+import Foundation
+
+enum SelectStatus: Int16, CaseIterable {
+    case wiiApplied
+    case applied
+    case selection
+    
+    
+}
