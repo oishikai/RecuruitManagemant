804a0b10f0b1bb1e1064401b05e52d6b074e86e5
Kai Oishi <bokuchan.kai@gmail.com>
2021-05-24 22:53:39 +0900

    selectStatus型にnameプロパティ追加

--- RecuruitManagement/SelectStatus.swift
+++ RecuruitManagement/SelectStatus.swift
@@ -8,9 +8,16 @@
 import Foundation
 
 enum SelectStatus: Int16, CaseIterable {
+    
     case wiiApplied
     case applied
     case selection
     
-    
+    var name:String {
+        switch self {
+        case .wiiApplied: return "未エントリー"
+        case .applied: return "エントリー済"
+        case .selection: return "選考中"
+        }
+    }
 }
